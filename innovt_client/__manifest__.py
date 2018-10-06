82cf6b35f2bb7fd3728cd2f346fc6fcec2ef9666
José Hernández <jhbez@outlook.com>
2018-10-05 23:47:03 -0500

    rem | innovt_client | unnecessary files and code

--- innovt_client/__manifest__.py
+++ innovt_client/__manifest__.py
@@ -23,9 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/res_company.xml',
-        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
