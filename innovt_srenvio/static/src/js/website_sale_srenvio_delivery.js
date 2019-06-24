b1fcdd8c6fdc5faeb59fe146a0256f4be6b043d2
José Hernández <jhbez@outlook.com>
2019-06-24 10:14:47 -0500

    Files Add License GPLv3
    One line program/script/file set copyright
    
    Reported-by: innov-ce
    Signed-off-by: José Hernández <jhbez@outlook.com>

--- innovt_srenvio/static/src/js/website_sale_srenvio_delivery.js
+++ innovt_srenvio/static/src/js/website_sale_srenvio_delivery.js
@@ -1,4 +1,21 @@
+/*
+   Copyright (C) 2019  MAXS
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 'use strict';
+
 odoo.define('website_sale_srenvio_delivery.checkout', function (require) {
 
     require('web.dom_ready');
